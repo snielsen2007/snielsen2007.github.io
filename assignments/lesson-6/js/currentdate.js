d4dd45a2109a7f5de43ed9e84914165c526792c2
snielsen2007 <snielsen@local.imodules.com>
2019-02-15 23:54:07 -0600

    hamburger attempt

---
+++ assignments/lesson-6/js/currentdate.js
@@ -0,0 +1,20 @@
+//Wednesday, 13 February 2019
+
+let today = new Date();
+let date = today.getFullYear() + get;
+document.getElementById('currentdate').innerHTML = date;
+
+let today = new Date();
+let year = today.getFullYear();
+let month = today.getMonth();
+//convert to text
+let monthname = "January";
+document.getElementById('currentdate').innerHTML = currentdate;
+
+    var d = new Date();
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+document.getElementById("demo").innerHTML = months[d.getMonth()];
+    
+var d = new Date();
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+document.getElementById("demo").innerHTML = days[d.getDay()];
