76d227c237412fabded6198ce78881d2c7b6e9e5
snielsen2007 <snielsen@local.imodules.com>
2019-02-16 10:54:46 -0600

    currentdate fixed

--- assignments/lesson-6/js/currentdate.js
+++ assignments/lesson-6/js/currentdate.js
@@ -1,5 +1,5 @@
 //Wednesday, 13 February 2019
-function currentDate() {
+//notice how this does not need to be placed in a function
     let fulldate = new Date();
     let day = fulldate.getDate();
     let numericmonth = fulldate.getMonth();
@@ -8,5 +8,4 @@ function currentDate() {
     let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-    document.getElementById("currentdate").innerHTML = weekday[numericweekday] + ", "+day +month[numericmonth]+" "+year;
-}
+    document.getElementById('currentdate').innerHTML = weekday[numericweekday] + ", "+day +" "+month[numericmonth]+" "+year;
