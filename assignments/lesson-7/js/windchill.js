a10aa5790c678d1324a61f046a9c4576df98bff8
snielsen2007 <snielsen@local.imodules.com>
2019-02-22 17:14:08 -0600

    js

--- assignments/lesson-7/js/windchill.js
+++ assignments/lesson-7/js/windchill.js
@@ -3,18 +3,15 @@
 * Output: return the wind chill facotr in Fahrenheit
 */
 
-function calculateWindChill(){
     let tempF = parseFloat(document.getElementById('temperaturevalue').innerHTML);
     let speed = parseFloat(document.getElementById('windspeedvalue').innerHTML);
     let result = windChill(tempF,speed);
     document.getElementById('windchillvalue').innerHTML = result;
-}
-                        
+                  
 function windChill (tempF, speed) {
 //set variables
     let t = tempF;
-    let s = speed;
-                        
+    let s = speed;                     
 //calculate wind chill
     let wc = 35.74 + 0.6215 * t - 35.75 * Math.pow( s, 0.16 ) + 0.4275 * t * Math.pow(s,0.16);                        
 //return result
