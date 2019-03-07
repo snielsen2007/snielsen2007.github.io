bcf3c68e4f06106b165b753bc34ff477d423b26b
snielsen2007 <snielsen@local.imodules.com>
2019-03-07 16:12:37 -0600

    towndata

--- assignments/lesson-9/js/towndata.js
+++ assignments/lesson-9/js/towndata.js
@@ -1,52 +1,46 @@
-    let header = document.querySelector('header');
-    let section = document.querySelector('section');
-    let requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
-    let request = new XMLHttpRequest();
-    request.open('GET', requestURL);
-    request.responseType = 'json';
-        request.send();
-        request.onload = function () {
-        let superHeroes = request.response;
-    populateHeader(superHeroes);
-    showHeroes(superHeroes);
-    }
-        function populateHeader(jsonObj) {
-                let myH1 = document.createElement('h1');
+let header = document.querySelector('header');
+let section = document.querySelector('section');
+let requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function () {
+    let resultJson = request.response;
+    console.log(resultJson)
+    populateHeader(resultJson);
+    showTown(resultJson.towns);
+}
+function populateHeader(jsonObj) {
+    let myH1 = document.createElement('h1');
     myH1.textContent = jsonObj['squadName'];
     header.appendChild(myH1);
 
     let myPara = document.createElement('p');
-    myPara.textContent = 'Hometown: ' + jsonObj['homeTown'] + ' // Formed: ' + jsonObj['formed'];
+    myPara.textContent = 'Name: ' + jsonObj['homeTown'] + ' // Formed: ' + jsonObj['formed'];
     header.appendChild(myPara);
 }
-        function showHeroes(jsonObj) {
-                let heroes = jsonObj['members'];
+function showTown(townList) {
+    
+    let mytempdiv = document.getElementById('tempdiv'); 
+    for (let i = 0; i < townList.length; i++) {
+        let myArticle = document.createElement('article');
+        let myH2 = document.createElement('h2');
+        let myPara1 = document.createElement('p');
+        let myPara2 = document.createElement('p');
+        let myPara3 = document.createElement('p');
+        let myList = document.createElement('ul');
 
-                for (let i = 0; i < heroes.length; i++) {
-                    let myArticle = document.createElement('article');
-    let myH2 = document.createElement('h2');
-    let myPara1 = document.createElement('p');
-    let myPara2 = document.createElement('p');
-    let myPara3 = document.createElement('p');
-    let myList = document.createElement('ul');
+        myH2.textContent = townList[i].name;
+        myPara1.textContent = 'Year Founded: ' + townList[i].yearFounded;
+        myPara2.textContent = 'Current Population: ' + townList[i].currentPopulation;
+        myPara3.textContent = 'Superpowers:';
 
-    myH2.textContent = heroes[i].name;
-    myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
-    myPara2.textContent = 'Age: ' + heroes[i].age;
-    myPara3.textContent = 'Superpowers:';
+        
 
-    let superPowers = heroes[i].powers;
-                    for (let j = 0; j < superPowers.length; j++) {
-                        let listItem = document.createElement('li');
-    listItem.textContent = superPowers[j];
-    myList.appendChild(listItem);
-}
-
-myArticle.appendChild(myH2);
-myArticle.appendChild(myPara1);
-myArticle.appendChild(myPara2);
-myArticle.appendChild(myPara3);
-myArticle.appendChild(myList);
-
-section.appendChild(myArticle);
+        mytempdiv.appendChild(myH2);
+        mytempdiv.appendChild(myPara1);
+        mytempdiv.appendChild(myPara2);
+        mytempdiv.appendChild(myPara3);
+    }
 }
