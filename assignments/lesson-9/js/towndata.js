c9700a4075ec0e310eb33e2e885252ec1d7b7aa1
snielsen2007 <snielsen@local.imodules.com>
2019-03-07 13:48:41 -0600

    let

---
+++ assignments/lesson-9/js/towndata.js
@@ -0,0 +1,52 @@
+    let header = document.querySelector('header');
+    let section = document.querySelector('section');
+    let requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+        request.send();
+        request.onload = function () {
+        let superHeroes = request.response;
+    populateHeader(superHeroes);
+    showHeroes(superHeroes);
+    }
+        function populateHeader(jsonObj) {
+                let myH1 = document.createElement('h1');
+    myH1.textContent = jsonObj['squadName'];
+    header.appendChild(myH1);
+
+    let myPara = document.createElement('p');
+    myPara.textContent = 'Hometown: ' + jsonObj['homeTown'] + ' // Formed: ' + jsonObj['formed'];
+    header.appendChild(myPara);
+}
+        function showHeroes(jsonObj) {
+                let heroes = jsonObj['members'];
+
+                for (let i = 0; i < heroes.length; i++) {
+                    let myArticle = document.createElement('article');
+    let myH2 = document.createElement('h2');
+    let myPara1 = document.createElement('p');
+    let myPara2 = document.createElement('p');
+    let myPara3 = document.createElement('p');
+    let myList = document.createElement('ul');
+
+    myH2.textContent = heroes[i].name;
+    myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
+    myPara2.textContent = 'Age: ' + heroes[i].age;
+    myPara3.textContent = 'Superpowers:';
+
+    let superPowers = heroes[i].powers;
+                    for (let j = 0; j < superPowers.length; j++) {
+                        let listItem = document.createElement('li');
+    listItem.textContent = superPowers[j];
+    myList.appendChild(listItem);
+}
+
+myArticle.appendChild(myH2);
+myArticle.appendChild(myPara1);
+myArticle.appendChild(myPara2);
+myArticle.appendChild(myPara3);
+myArticle.appendChild(myList);
+
+section.appendChild(myArticle);
+}
