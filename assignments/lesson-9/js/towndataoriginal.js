48ff6f331bf64d405d542933d7a01f95ca7c19b7
snielsen2007 <snielsen@local.imodules.com>
2019-03-08 09:30:25 -0600

    towndata

--- assignments/lesson-9/js/towndataoriginal.js
+++ assignments/lesson-9/js/towndataoriginal.js
@@ -1,28 +1,16 @@
-//grab references to the <header> and <section> elements and store them in variables
 let header = document.querySelector('header');
 let section = document.querySelector('section');
-//use an API called XMLHttpRequest --a JavaScript object that allows us to make network requests to retrieve resources from a server via JavaScript
-// store the URL of the JSON we want to retrieve in a variable
 let requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
-//create a new request object instance from the XMLHttpRequest constructor, using the new keyword
 let request = new XMLHttpRequest();
-//open a new request using the open() method
 request.open('GET', requestURL);
-//set the responseType to JSON, so that XHR knows that the server will be returning JSON, and that this should be converted behind the scenes into a JavaScript object.
 request.responseType = 'json';
-//send the request with the send() method
 request.send();
-//store the response to our request (available in the response property) in a variable; 
-//this variable will now contain the JavaScript object based on the JSON! 
-//Then pass that object to two function calls — the first one will fill the < header > with the correct data, 
-//the second one will create an information card for each town , and insert it into the < section >
 request.onload = function () {
     let resultJson = request.response;
     console.log(resultJson)
     populateHeader(resultJson);
     showTown(resultJson.towns);
 }
-//populate the header
 function populateHeader(jsonObj) {
     let myH1 = document.createElement('h1');
     myH1.textContent = jsonObj['squadName'];
