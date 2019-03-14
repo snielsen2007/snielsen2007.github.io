34a8d7898bf3e43b848ca56ba278eeca82b842b5
snielsen2007 <snielsen@local.imodules.com>
2019-03-14 14:56:18 -0500

    weatherapi

---
+++ assignments/lesson-10/js/prestonweatherapi.js
@@ -0,0 +1,19 @@
+let weatherRequest = new XMLHttpRequest();
+apiURLstring='https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=be51ac7443b3bdad44d5779b4d1a2133';
+weatherRequest.open('Get', apiURLstring, true);
+weatherRequest.send();
+
+weatherRequest.onload = function () {
+    let weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+    //document.getElementById('cc-temp').innerHTML = weatherData.main.temp;
+    let icon = "http://openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png"//add loop so if there is more than one icon, it pulls both
+    let desc = weatherData.weather[0].description;
+
+   // document.getElementById('cc-img').setAttribute('src', icon);
+   // document.getElementById('cc-img').setAttribute('alt', desc);
+    document.getElementById('descriptionvalue').innerHTML = weatherData.weather.description;
+    document.getElementById('temperaturevalue').innerHTML = weatherData.main.temp;
+    document.getElementById('humidityvalue').innerHTML = weatherData.main.humidity;
+    document.getElementById('windspeedvalue').innerHTML = weatherData.wind.speed;
+}
---
+++ assignments/lesson-10/js/weatherapi.js
@@ -0,0 +1,15 @@
+let weatherRequest = new XMLHttpRequest();
+apiURLstring='https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=be51ac7443b3bdad44d5779b4d1a2133';
+weatherRequest.open('Get', apiURLstring, true);
+weatherRequest.send();
+
+weatherRequest.onload = function () {
+    let weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+    document.getElementById('cc-temp').innerHTML = weatherData.main.temp;
+    let icon = "http://openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png"
+    let desc = weatherData.weather[0].description;
+
+    document.getElementById('cc-img').setAttribute('src', icon);
+    document.getElementById('cc-img').setAttribute('alt', desc);
+}
