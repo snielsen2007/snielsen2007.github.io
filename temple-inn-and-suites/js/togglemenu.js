d4bdeb6c0136400035aea648fc57596671f67c98
snielsen2007 <snielsen@local.imodules.com>
2019-04-06 20:38:13 -0500

    navigation

--- temple-inn-and-suites/js/togglemenu.js
+++ temple-inn-and-suites/js/togglemenu.js
@@ -1,3 +1,4 @@
 function toggleMenu(){
     document.getElementsByClassName("site-navigation")[0].classList.toggle("responsive");
+    document.getElementsByClassName("headercontainer")[0].classList.toggle("responsive");
 }
