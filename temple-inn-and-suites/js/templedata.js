ed0882877baac97b1bdf47cf306a8c4ae1caa65d
snielsen2007 <snielsen@local.imodules.com>
2019-04-07 19:46:42 -0500

    changes

--- temple-inn-and-suites/js/templedata.js
+++ temple-inn-and-suites/js/templedata.js
@@ -1,5 +1,5 @@
 //define the container to build within
-let townfacts = document.querySelector('#templefactscontainer');
+let closureschedule = document.querySelector('#templeclosureschedule');
 let section = document.querySelector('section');
 //use an API called XMLHttpRequest --a JavaScript object that allows us to make network requests to retrieve resources from a server via JavaScript
 // store the URL of the JSON we want to retrieve in a variable
@@ -17,62 +17,22 @@ request.send();
 //Then pass that object to function calls
 request.onload = function () {
     let responsedata = request.response;
-    let townList = responsedata['towns'];
-    for (let i = 0; i < townList.length; i++) {
-
-        if (townList[i].name == "Fish Haven" || townList[i].name == "Preston" || townList[i].name == "Soda Springs") {
-
-
-            let myArticleContainer = document.createElement('div')
-            let myArticle = document.createElement('article');
-            let myH2 = document.createElement('h2');
-            let myPara1 = document.createElement('p');
-            let myPara2 = document.createElement('p');
-            let mySpan2a = document.createElement('span');
-            let mySpan2b = document.createElement('span');
-            let myPara3 = document.createElement('p');
-            let mySpan3a = document.createElement('span');
-            let mySpan3b = document.createElement('span');
-            let myPara4 = document.createElement('p');
-            let mySpan4a = document.createElement('span');
-            let mySpan4b = document.createElement('span');
-            let myImg = document.createElement('img');
-            let link = townList[i].name;
-            let linkname = link.toLowerCase();   // this makes it all lowercase
-
-
-            myH2.textContent = townList[i].name;
-            myPara1.textContent = townList[i].motto;
-            mySpan2a.textContent = 'Year Founded: ';
-            mySpan2b.textContent = townList[i].yearFounded;
-            mySpan3a.textContent = 'Population: '
-            mySpan3b.textContent = townList[i].currentPopulation;
-            mySpan4a.textContent = 'Annual Rain Fall: ';
-            mySpan4b.textContent = townList[i].averageRainfall + ' "';
-            link = link.replace(/\s/g, '');  // this is regex to remove the spaces
-            myImg.setAttribute('src', 'images/towndata/' + linkname + '300px' + '.jpg');
-            myImg.setAttribute('alt', 'The city of ' + townList[i].name + '  picture');
-            myPara1.setAttribute('class', 'motto');
-            mySpan2a.setAttribute('class', 'towndatalabel');
-            mySpan3a.setAttribute('class', 'towndatalabel');
-            mySpan4a.setAttribute('class', 'towndatalabel');
-
-            myArticle.appendChild(myH2);
-            myArticle.appendChild(myPara1);
-            myArticle.appendChild(myPara2);
-            myPara2.appendChild(mySpan2a);
-            myPara2.appendChild(mySpan2b);
-            myArticle.appendChild(myPara3);
-            myPara3.appendChild(mySpan3a);
-            myPara3.appendChild(mySpan3b);
-            myArticle.appendChild(myPara4);
-            myPara4.appendChild(mySpan4a);
-            myPara4.appendChild(mySpan4b);
-            myArticle.appendChild(myImg);
-
-            townfacts.appendChild(myArticleContainer);
-            myArticleContainer.appendChild(myArticle);
-
+    let templeList = responsedata['temples'];
+    let templeTitle = document.getElementById('templetitle').innerHTML;
+    for (let i = 0; i < templeList.length; i++) {
+        if (templeList[i].name == templeTitle) {
+            let closureList = templeList[i].templeclosureSchedule;
+            let closureItem = 0;
+            for (let j = 0; j < closureList.length; j++) {
+                let closure = closureList[closureItem];
+                closureItem++;
+        //create HTL content on the fly
+                let myArticle = document.createElement('article');
+                let myPara = document.createElement('p');
+                myPara.textContent = closure; 
+                myArticle.appendChild('myPara');
+                closureschedule.appendChild(myArticle);
+            }
         }
     }
 }
