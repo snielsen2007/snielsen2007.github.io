6b4808e77d4b6b1370f2f145ecd829d13651a29b
snielsen2007 <snielsen@local.imodules.com>
2019-04-12 22:39:53 -0500

    html changes

--- temple-inn-and-suites/js/currentweather.js
+++ temple-inn-and-suites/js/currentweather.js
@@ -48,6 +48,16 @@ weatherRequest.onload = function () {
     }
 }
 
+function windChill(tempF, speed) {
+    //set variables
+    let t = tempF;
+    let s = speed;
+    //calculate wind chill
+    let wc = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
+    //return result
+    return wc;
+}
+
 
 /*}
 weatherStats;
